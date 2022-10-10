b3b34c9f15f1b0981d87129374aa99a3435037f8
Natali <n7483205@ya.ru>
2022-10-10 20:48:08 +0700

    lines deleted

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿/* выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-645 -> 5,    78 -> третьей цифры нет,     32679 -> 6 */
-
-Console.WriteLine("Введите любое число.");
+﻿Console.WriteLine("Введите любое число.");
 int number = int.Parse(Console.ReadLine()!);
 
 if (number <= 99)
@@ -14,25 +11,3 @@ else
     Console.WriteLine("Третья цифра числа: ");
     Console.WriteLine(str[2]);
 }
-
-//Console.WriteLine(str);
-
-
-/* int i = 10;
-string str = i.ToString();
-
-
-int index = 0;
-int num = num[0];
-int result = num[2];
-
-if (num <= 1)
-{
-    Console.WriteLine("Третьей цифры нет.");
-}
-
-if (num > 1)
-{
-    Console.WriteLine("Третья цифра:");
-    Console.Write(num[3]);
-} */
